0a228f16e27b4faa0e19d3931b8c0d4ae0bfad5b
unknown <mingmartmg252@gmail.com>
2025-08-04 23:28:27 +0545

    Initial commit

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'mingmar2019',  // your password
+  password: 'YourPassword',  // your password
   database: 'note',
   waitForConnections: true,
   connectionLimit: 10,
